842455df4a9d42215ce0f173c16336928a385eb4
yqman <yqman@yahoo.com>
2023-06-06 08:50:11 +0800

    modify.....

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+apt get install -y libgnutls28-dev libev-dev ibpam0g-dev liblz4-dev libseccomp-dev \
+        libreadline-dev libnl-route-3-dev libkrb5-dev libradcli-dev \
+        libcurl4-gnutls-dev libcjose-dev libjansson-dev libprotobuf-c-dev \
+        libtalloc-dev libhttp-parser-dev protobuf-c-compiler gperf \
+        nuttcp lcov libuid-wrapper libpam-wrapper libnss-wrapper \
+        libsocket-wrapper gss-ntlmssp haproxy iputils-ping freeradius \
+        gawk gnutls-bin iproute2 yajl-tools tcpdump

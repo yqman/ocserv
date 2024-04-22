a4e6012a7d0c02280e4c2dcbdd7acd215b97c871
yqman <yqman@yahoo.com>
2024-04-22 08:53:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ apt-get install -y libgnutls28-dev libev-dev libpam0g-dev liblz4-dev libseccomp-
         nuttcp lcov libuid-wrapper libpam-wrapper libnss-wrapper \
         libsocket-wrapper gss-ntlmssp haproxy iputils-ping freeradius \
         gawk gnutls-bin iproute2 yajl-tools tcpdump
-sudo iptables -t nat -o eth0 -A POSTROUTING -j MASQUERADE
+sudo iptables -t nat -o enpls0 -A POSTROUTING -j MASQUERADE

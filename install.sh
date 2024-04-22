50573b08a8d6a9ea8e05e56ff2cfb9d4d1b2f2de
yqman <yqman@yahoo.com>
2024-04-22 09:37:33 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,9 @@ apt-get install -y libgnutls28-dev libev-dev libpam0g-dev liblz4-dev libseccomp-
         nuttcp lcov libuid-wrapper libpam-wrapper libnss-wrapper \
         libsocket-wrapper gss-ntlmssp haproxy iputils-ping freeradius \
         gawk gnutls-bin iproute2 yajl-tools tcpdump
+#sysctl -w net.ipv4.ip_forward =1
 #sudo iptables -t nat -o eth0 -A POSTROUTING -j MASQUERADE
+#apt-get install iptables-persistent
+#netfilter-persistent save
+#iptables -I INPUT -p tcp --dport 443 -j ACCEPT
+#iptables -I INPUT -p udp --dport 443 -j ACCEPT
